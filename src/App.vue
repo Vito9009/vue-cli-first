cef1a31dfb8c16e68388744cb6ba6f0102af1884
Your Name <yan.vi193@gmail.com>
2022-01-26 16:07:12 +0100

    stampa-frase

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyVueProject />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyVueProject from './components/MyVueProject.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyVueProject
   }
 }
 </script>
---
+++ src/components/MyVueProject.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+      <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyVueProject',
+}
+</script>
+
+<style scoped>
+
+</style>
